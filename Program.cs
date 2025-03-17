e68c1bd23262dfc5210dd4a1eb00835d9a9dabfb
Miguel <mighuel.perassoli@gmail.com>
2025-03-17 19:34:59 -0300

    Finalização tarefa Adivinha

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿
+﻿//Atividade Adivinha
+
+Console.Clear();
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("+---------------------+");
+Console.Write("|");
+Console.ResetColor();
+Console.Write(" O que é o que é...");
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("  |");
+Console.WriteLine("+---------------------+");
+Console.ResetColor();
+Thread.Sleep(1200);
+Console.WriteLine(@"Quanto mais rugas têm mais novo é");
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.Write(".");
+Thread.Sleep(1000);
+Console.Write(".");
+Thread.Sleep(1000);
+Console.WriteLine(".");
+Console.ResetColor();
+Thread.Sleep(3000);
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("O PNEU");
+Console.ResetColor();
