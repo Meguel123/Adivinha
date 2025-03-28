e103222a9f0859029ae7b73195835a737c08795c
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-03-28 20:24:16 -0300

    pronto

--- Program.cs
+++ Program.cs
@@ -3,25 +3,30 @@
 
 Console.Clear();
 
-Console.ForegroundColor = ConsoleColor.Yellow;
+Console.ForegroundColor = ConsoleColor.Blue;
 Console.WriteLine("+---------------------+");
+
 Console.Write("|");
+
 Console.ResetColor();
 Console.Write(" O que é o que é...");
-Console.ForegroundColor = ConsoleColor.Yellow;
+
+Console.ForegroundColor = ConsoleColor.Blue;
+
 Console.WriteLine("  |");
+
 Console.WriteLine("+---------------------+");
 Console.ResetColor();
-Thread.Sleep(1200);
+Thread.Sleep(2000);
 Console.WriteLine(@"Quanto mais rugas têm mais novo é");
-Console.ForegroundColor = ConsoleColor.Yellow;
+Console.ForegroundColor = ConsoleColor.Red;
 Console.Write(".");
 Thread.Sleep(1000);
 Console.Write(".");
 Thread.Sleep(1000);
 Console.WriteLine(".");
 Console.ResetColor();
-Thread.Sleep(3000);
+Thread.Sleep(2000);
 Console.ForegroundColor = ConsoleColor.Green;
 Console.WriteLine("O PNEU");
 Console.ResetColor();
